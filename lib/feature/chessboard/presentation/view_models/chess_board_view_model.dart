a33b99f8b25ed6d9db94fa9d13824d8528815d1f
pogrebnyak-kirill <pogrebnyak@spiks.ru>
2024-03-14 13:03:02 +0300

    refactor/valid_moves

--- lib/feature/chessboard/presentation/view_models/chess_board_view_model.dart
+++ lib/feature/chessboard/presentation/view_models/chess_board_view_model.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
 import 'dart:developer';
 
 import 'package:chess316/core/chess_icons/chess_icons.dart';
+import 'package:chess316/feature/chessboard/data/services/validate_moves.dart';
 import 'package:chess316/feature/chessboard/domain/models/chess_piece.dart';
 import 'package:chess316/feature/chessboard/domain/models/chess_piece_enum.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,12 +9,10 @@ import 'package:flutter/cupertino.dart';
 class ChessBoardViewModel extends ChangeNotifier {
   List<List<ChessPiece?>> _chessBoard = [];
   List<bool> _selectedPieces = [];
-  bool _isSelected = false;
+  List<bool> _validMoves = [];
   int _selectedFieldRow = -1;
   int _selectedFieldColumn = -1;
   int _selectedFieldIndex = -1;
-
-  bool get isSelected => _isSelected;
 
   int get selectedFieldIndex => _selectedFieldIndex;
 
@@ -25,6 +23,8 @@ class ChessBoardViewModel extends ChangeNotifier {
   List<bool> get selectedPieces => _selectedPieces;
 
   List<List<ChessPiece?>> get chessBoard => _chessBoard;
+
+  List<bool> get validMoves => _validMoves;
 
   ChessBoardViewModel() {
     initChessBoard();
@@ -39,6 +39,11 @@ class ChessBoardViewModel extends ChangeNotifier {
       ),
     );
     List<bool> initSelectedPieces = List.generate(
+      64,
+      (index) => false,
+    );
+
+    List<bool> initValidMoves = List.generate(
       64,
       (index) => false,
     );
@@ -86,34 +91,66 @@ class ChessBoardViewModel extends ChangeNotifier {
     initBoard[0][3] = ChessPiece(
         type: ChessPieceType.queen, icon: ChessIcons.queen, isWhite: false);
     initBoard[0][4] = ChessPiece(
-        type: ChessPieceType.king, icon: ChessIcons.queen, isWhite: false);
+        type: ChessPieceType.king, icon: ChessIcons.king, isWhite: false);
     initBoard[7][4] = ChessPiece(
-        type: ChessPieceType.queen, icon: ChessIcons.king, isWhite: true);
+        type: ChessPieceType.queen, icon: ChessIcons.queen, isWhite: true);
     initBoard[7][3] = ChessPiece(
         type: ChessPieceType.king, icon: ChessIcons.king, isWhite: true);
 
     _chessBoard = initBoard;
     _selectedPieces = initSelectedPieces;
+    _validMoves = initValidMoves;
     notifyListeners();
   }
 
   void selectPiece(int index) {
-    log(index.toString());
     if (_selectedPieces[index]) {
       _selectedFieldIndex = -1;
       _selectedFieldRow = -1;
       _selectedFieldColumn = -1;
       _selectedPieces[index] = false;
+      for (int i = 0; i < 64; i++) {
+        _validMoves[i] = false;
+      }
     } else {
       final row = index ~/ 8;
       final column = index % 8;
+
+      if (_validMoves[index] == true &&
+          _chessBoard[_selectedFieldRow][_selectedFieldColumn] != null) {
+        movePiece(index);
+      }
+
       if (_chessBoard[row][column] != null) {
+        for (int i = 0; i < 64; i++) {
+          _selectedPieces[i] = false;
+          _validMoves[i] = false;
+        }
+
         _selectedFieldIndex = index;
         _selectedFieldRow = index ~/ 8;
         _selectedFieldColumn = index % 8;
         _selectedPieces[index] = true;
+
+        final ChessPiece piece =
+            chessBoard[_selectedFieldRow][_selectedFieldColumn]!;
+
+        final List<List<int>> moves = CalculateValidMoves().calculateValidMoves(
+            _selectedFieldRow, _selectedFieldColumn, piece, chessBoard);
+
+        for (var move in moves) {
+          _validMoves[move[0] * 8 + move[1]] = true;
+        }
       }
     }
     notifyListeners();
+  }
+
+  void movePiece(int index) {
+    final row = index ~/ 8;
+    final column = index % 8;
+    _chessBoard[row][column] =
+        _chessBoard[_selectedFieldRow][_selectedFieldColumn];
+    _chessBoard[_selectedFieldRow][_selectedFieldColumn] = null;
   }
 }
