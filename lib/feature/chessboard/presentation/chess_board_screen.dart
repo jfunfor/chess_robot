ade9c0b3b3a2950924d02e0d70d9689be56317e9
pogrebnyak-kirill <pogrebnyak@spiks.ru>
2024-04-02 17:56:31 +0300

    refactor/check & checkMate fix

--- lib/feature/chessboard/presentation/chess_board_screen.dart
+++ lib/feature/chessboard/presentation/chess_board_screen.dart
@@ -2,11 +2,10 @@ import 'dart:math';
 
 import 'package:chess316/feature/chessboard/domain/models/chess_pieces.dart';
 import 'package:chess316/feature/chessboard/presentation/view_models/chess_board_view_model.dart';
+import 'package:chess316/feature/chessboard/presentation/widgets/alert_dialog_content.dart';
 import 'package:chess316/feature/chessboard/presentation/widgets/chess_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import '../../../core/styles/colors.dart';
 
 class ChessBoardPage extends StatelessWidget {
   const ChessBoardPage({super.key});
@@ -42,110 +41,130 @@ class _ChessBoardScreenState extends State<ChessBoardScreen> {
   }
 
   @override
+  void initState() {
+    widget.model.addListener(() {
+      if (widget.model.checkMate) {
+        _showResetGameDialog(context, onConfirmTap: () {
+          widget.model.restartGame();
+          Navigator.of(context)
+            ..pop()
+            ..pop();
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ChessBoardScreen oldWidget) {
+    oldWidget.model.dispose();
+    widget.model.addListener(() {
+      if (widget.model.checkMate) {
+        _showResetGameDialog(context, onConfirmTap: () {
+          widget.model.restartGame();
+          Navigator.of(context)
+            ..pop()
+            ..pop();
+        });
+      }
+    });
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown[100],
       body: Center(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return Container(
-              constraints: BoxConstraints(
-                maxWidth: min(600, constraints.maxHeight-220),
-              ),
-              alignment: Alignment.center,
-              margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ListenableBuilder(
-                      listenable: widget.model,
-                      builder: (context, _) {
-                        return Text(
-                          !widget.model.isWhiteTurn &&
-                                  widget.model.alertMessage.isEmpty
-                              ? 'Black`s turn'
-                              : widget.model.alertMessage,
-                          style: const TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        );
-                      }),
-                  const SizedBox(
-                    height: 20,
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Container(
+            constraints: BoxConstraints(
+              maxWidth: min(600, constraints.maxHeight - 220),
+            ),
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ListenableBuilder(
+                    listenable: widget.model,
+                    builder: (context, _) {
+                      return Text(
+                        !widget.model.isWhiteTurn &&
+                                widget.model.alertMessage.isEmpty
+                            ? 'Black`s turn'
+                            : widget.model.alertMessage,
+                        style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      );
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 64,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 8,
                   ),
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 64,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 8,
-                    ),
-                    itemBuilder: (context, index) {
-                      return ListenableBuilder(
-                          listenable: widget.model,
-                          builder: (context, _) {
-                            return ChessField(
-                              isFilled: isFieldFilled(index),
-                              piece: getPiece(index),
-                              isSelected:
-                                  widget.model.selectedFieldIndex == index,
-                              onTap: () {
-                                widget.model.selectPiece(index);
-                              },
-                              isValidMove: widget.model.validMoves[index],
-                            );
-                          });
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ListenableBuilder(
-                      listenable: widget.model,
-                      builder: (context, _) {
-                        return Text(
-                          widget.model.isWhiteTurn ? 'White`s turn' : ' ',
-                          style: const TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        );
-                      }),
-                  const SizedBox(height: 20),
-                  if (widget.model.check)
+                  itemBuilder: (context, index) {
+                    return ListenableBuilder(
+                        listenable: widget.model,
+                        builder: (context, _) {
+                          return ChessField(
+                            isFilled: isFieldFilled(index),
+                            piece: getPiece(index),
+                            isSelected:
+                                widget.model.selectedFieldIndex == index,
+                            onTap: () {
+                              widget.model.selectPiece(index);
+                            },
+                            isValidMove: widget.model.validMoves[index],
+                          );
+                        });
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ListenableBuilder(
+                    listenable: widget.model,
+                    builder: (context, _) {
+                      return Text(
+                        widget.model.isWhiteTurn &&
+                                widget.model.alertMessage.isEmpty
+                            ? 'White`s turn'
+                            : widget.model.alertMessage,
+                        style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      );
+                    }),
+                const SizedBox(height: 20),
+                CupertinoButton(
+                  onPressed: () {
                     _showResetGameDialog(context, onConfirmTap: () {
                       widget.model.restartGame();
                       Navigator.of(context).pop();
-                    }),
-                  ListenableBuilder(
-                      listenable: widget.model,
-                      builder: (context, _) {
-                        return Text(
-                          'check: ${widget.model.check}',
-                        );
-                      }),
-                  CupertinoButton(
-                    onPressed: () {
-                      _showResetGameDialog(context, onConfirmTap: () {
-                        widget.model.restartGame();
-                        Navigator.of(context).pop();
-                      });
-                    },
-                    minSize: 0,
-                    padding: EdgeInsets.zero,
-                    child: const Text(
-                      'Reset game?',
-                      style: TextStyle(
-                        color: Colors.brown,
-                      ),
+                    });
+                  },
+                  minSize: 0,
+                  padding: EdgeInsets.zero,
+                  child: const Text(
+                    'Reset game?',
+                    style: TextStyle(
+                      color: Colors.brown,
                     ),
                   ),
-                ],
-              ),
-            );
-          }
-        ),
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
@@ -157,53 +176,8 @@ _showResetGameDialog(BuildContext context,
       useRootNavigator: false,
       context: context,
       builder: (context) {
-        return AlertDialog(
-          contentPadding: const EdgeInsets.all(20.0).copyWith(top: 40),
-          backgroundColor: Colors.brown[100],
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Play again?',
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OutlinedButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(AppColors.darkBrown),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(AppColors.white),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('No'),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  OutlinedButton(
-                    onPressed: onConfirmTap,
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(AppColors.darkBrown),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(AppColors.white),
-                    ),
-                    child: const Text('Yes'),
-                  ),
-                ],
-              ),
-            ],
-          ),
+        return AlertDialogContent(
+          onConfirmTap: onConfirmTap,
         );
       });
 }
---
+++ lib/feature/chessboard/presentation/widgets/alert_dialog_content.dart
@@ -0,0 +1,65 @@
+import 'package:chess316/core/styles/colors.dart';
+import 'package:flutter/material.dart';
+
+class AlertDialogContent extends StatelessWidget {
+  final VoidCallback onConfirmTap;
+
+  const AlertDialogContent({
+    super.key,
+    required this.onConfirmTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      contentPadding: const EdgeInsets.all(20.0).copyWith(top: 40),
+      backgroundColor: Colors.brown[100],
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Play again?',
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(AppColors.darkBrown),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(AppColors.white),
+                ),
+                onPressed: () {
+                  Navigator.of(context)
+                    ..pop()
+                    ..pop();
+                },
+                child: const Text('No'),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              OutlinedButton(
+                onPressed: onConfirmTap,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(AppColors.darkBrown),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(AppColors.white),
+                ),
+                child: const Text('Yes'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
