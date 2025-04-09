f4ad4e01842cc5194fa7eba01a53f2c1e3abea89
unknown <arseniyafonin681@gmail.com>
2025-04-09 15:25:04 +0300

    Добавление функции возврата доски в исходное состояние

--- backend/game.py
+++ backend/game.py
@@ -21,7 +21,8 @@ class Session:
         self.players = []
         self.is_active = False
         self.current_player = None
-        self.board = chess.Board()
+        self.chess = chess
+        self.board = self.chess.Board()#"8/8/8/8/2P5/8/Q7/8 w - - 0 1"
 
     def add_player(self, websocket):
         if not self.active_players:
@@ -63,3 +64,23 @@ class Session:
 
     def check_gameover(self):
         return self.board.is_game_over()
+    
+    def check_square(self,pos):
+        if self.board.piece_at(chess.parse_square(pos)) != None:
+            return  self.board.piece_at(chess.parse_square(pos))
+        else:
+            return None
+        
+    def get_remaining_pieces_with_squares(self):
+    
+        remaining_pieces = []
+    
+        for square in chess.SQUARES:
+            piece = self.board.piece_at(square)
+            if piece is not None:
+                square_name = chess.square_name(square)  # например, "e4"
+                piece_symbol = piece.symbol()    # например, "P"
+                remaining_pieces.append((square_name, piece_symbol))
+    
+        return remaining_pieces
+        
--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,8 @@ import asyncio
 import websockets
 import json
 
+import chess
+from itertools import chain, zip_longest
 from redis_conn import RedisConnector
 from robot_conn import RobotConnector
 from game import (
@@ -11,13 +13,226 @@ from game import (
 
 LETTERS = "abcdefgh"
 
+pos_board_2 = 0
 
 def change_format_cell(cell):
     return int(cell[1]) * 8 - (8 - (int(LETTERS.index(cell[0])) + 1))
 
-
+def change_format_cell_original(pos):
+      if 1 <= pos <= 64:  # Проверяем, что число в допустимом диапазоне
+            column = LETTERS[(pos - 1) % 8]  # Находим столбец
+            row = (pos - 1) // 8 + 1  # Находим строку
+            return f"{column}{row}"
+      
 def robot_request(board_from, pos_from, board_to, pos_to):
     return f"Move,{board_from},{pos_from},{board_to},{pos_to}\r\n"
+
+def decode_redis(data):
+    return data.decode("utf-8")
+
+def return_move(square,session: Session,robot,piece_positions): #добавить условие если возвращаеться со 2 доски
+
+    current_square = square[0]
+    figure = square[1]
+    Board = 1
+
+    if square[0][0:7] == "Board_2":#проверка на то что клетка со 2 доски
+        current_square = None
+        square_board_2 = square[0][7:len(square[0])]
+        Board = 2
+
+    flag = False
+
+    for square in piece_positions[figure]:#если фугура стоит на каком либо месте из своих возможных
+        if current_square == square:
+            flag = True
+
+        if flag == True:
+            break
+
+        
+        
+    
+    if flag == False:#идем сюда в том случае если фигура не стоит ни на каком-либо своем месте
+        for square in piece_positions[figure]:
+            flag = False
+            piece = session.board.piece_at(change_format_cell(square)-1) # проверка клетки на наличие какой либо фигуры
+        #проверяем можем ли мы поставить фигуру на клетку начинающуюю с начала списка
+
+            #если там другая фигура и не того же типа убираем ее на свое место
+            if piece != None and piece.symbol() != figure:
+                square = (square,piece.symbol())
+                return_move(square,session,robot,piece_positions)
+
+            
+            #если там никого нет, ставим
+            if piece == None:
+                if current_square != None:#если клетка не находиться на 2 доске
+                    session.board.remove_piece_at(change_format_cell(current_square)-1) #удаляем фигуру в логике в доске 1
+                else:
+                    current_square = square_board_2
+
+
+                session.board.set_piece_at(change_format_cell(square)-1,session.chess.Piece.from_symbol(figure)) # и добавляем ее
+
+                robot_response = robot.send_and_receive( #переставляем фигуру на 1 доске
+                robot_request(
+                    Board,
+                    change_format_cell(current_square),
+                    1,
+                    change_format_cell(square)
+                    )
+                )
+
+                if robot_response != "Done":
+                    raise Exception("The robot failed to make remove figure")
+
+                flag = True
+
+            if flag == True:#установили фигуру на свое место
+                break
+
+
+def return_moves_all(session: Session,robot,piece_positions):
+    remaining_piece = session.get_remaining_pieces_with_squares()
+    
+    for square in remaining_piece:
+        return_move(square,session,robot,piece_positions)
+        remaining_piece = session.get_remaining_pieces_with_squares()
+    
+
+def move_all_to_current_board(Move_w,Move_b,session: Session):
+
+    Move_w = list(reversed([move for move in Move_w if int(move[-1]) == 0]))
+    Move_b = list(reversed([move for move in Move_b if int(move[-1]) == 0]))
+
+    for w,b in zip_longest(Move_w,Move_b): #все ходы, чтобы прийти к текущему состоянию доски
+        if w != None:
+            session.make_move(w[0:2]+w[3:5])
+
+        if b!= None:
+            session.make_move(b[0:2]+b[3:5])
+
+    return session.chess
+    
+def return_piece_from_2_to_1(session: Session,robot,piece_positions,dataBase):
+    dead_pieces_white = [piece for piece in list(map(decode_redis,dataBase.lrange("WHITE",0,-1))) if int(piece[-1]) == 1]
+    dead_pieces_black = [piece for piece in list(map(decode_redis,dataBase.lrange("BLACK",0,-1))) if int(piece[-1]) == 1]
+
+    for piece in dead_pieces_white:
+        square = [f"Board_2{piece[3:5]}",piece[len(piece)-3]]
+        return_move(square,session,robot,piece_positions)
+    
+    for piece in dead_pieces_black:
+        square = [f"Board_2{piece[3:5]}",piece[len(piece)-3]]
+        return_move(square,session,robot,piece_positions)
+    
+
+def return_original(itemsWhite,itemsBlack,robot,dataBase):
+
+    piece_positions = {
+    # Белые фигуры
+    "P": ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],  # Пешки
+    "N": ["b1", "g1"],  # Кони
+    "B": ["c1", "f1"],  # Слоны
+    "R": ["a1", "h1"],  # Ладьи
+    "Q": ["d1"],  # Ферзь
+    "K": ["e1"],  # Король
+    
+    # Черные фигуры (строчные)
+    "p": ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],  # Пешки
+    "n": ["b8", "g8"],  # Кони
+    "b": ["c8", "f8"],  # Слоны
+    "r": ["a8", "h8"],  # Ладьи
+    "q": ["d8"],  # Ферзь
+    "k": ["e8"],  # Король
+    }
+    session = Session()
+
+    session.chess = move_all_to_current_board(itemsWhite,itemsBlack,session) #все ходы, чтобы прийти к текущему состоянию доски
+
+
+
+    return_moves_all(session,robot,piece_positions)#возврат оставшихся фигур на свои места
+
+    return_piece_from_2_to_1(session,robot,piece_positions,dataBase)
+
+def return_original_type_2(itemsWhite,itemsBlack,robot): #возврат доски в обратном порядке
+    for i in range(len(itemsWhite)):
+        change_board_w = int(itemsWhite[i][-1])
+        change_board_b =  int(itemsBlack[i][-1])
+
+        if change_board_w < change_board_b: #возврат сначала белой потом черной
+            robot_response = robot.send_and_receive( 
+                robot_request(
+                    1,
+                    change_format_cell(itemsWhite[i][0:2]),
+                    1,
+                    change_format_cell(itemsWhite[i][3:5])
+                )
+            )
+            
+            
+            print(itemsWhite[i],itemsBlack[i])
+
+
+            if robot_response != "Done":
+                raise Exception("The robot failed to make remove figure")
+            
+            robot_response = robot.send_and_receive( 
+                robot_request(
+                    2,
+                    change_format_cell(itemsBlack[i][0:2]),
+                    1,
+                    change_format_cell(itemsBlack[i][3:5])
+                )
+            )
+
+            if robot_response != "Done":
+                raise Exception("The robot failed to make remove figure")
+
+        else: #возврат черной потом белой
+            robot_response = robot.send_and_receive( 
+                robot_request(
+                    1,
+                    change_format_cell(itemsWhite[i][0:2]),
+                    1,
+                    change_format_cell(itemsWhite[i][3:5])
+                )
+            )
+
+            print(itemsBlack[i],itemsWhite[i])
+           
+            if robot_response != "Done":
+                raise Exception("The robot failed to make remove figure")
+            
+            robot_response = robot.send_and_receive( #
+                robot_request(
+                    2,
+                    change_format_cell(itemsBlack[i][0:2]),
+                    1,
+                    change_format_cell(itemsBlack[i][3:5])
+                )
+            )
+
+            if robot_response != "Done":
+                raise Exception("The robot failed to make remove figure")
+
+def return_board_to_original(): #возврат доски в исходное состояние
+    redis = RedisConnector()
+    redis.connect()
+    dataBase = redis.client
+
+    robot = RobotConnector()
+    robot.connect()
+
+    itemsWhite = list(reversed(list(map(decode_redis,dataBase.lrange("WHITE",0,-1)))))
+    itemsBlack = list(reversed(list(map(decode_redis,dataBase.lrange("BLACK",0,-1)))))
+
+    
+    return_original(itemsWhite,itemsBlack,robot,dataBase)
+
+
 
 
 class WebSocketServer:
@@ -55,12 +270,51 @@ class WebSocketServer:
             raise Exception(
                 "Mandatory fields pos_start and pos_end are missing"
             )
+        
+        piece_simbol_start = self.session.board.piece_at(change_format_cell(pos_start)-1).symbol()
+        print(piece_simbol_start)
+        
 
+        if  self.session.check_square(pos_end) != None: #проверка на занятость клетки фигурой
+            cell_occupied = True
+            piece_simbol_end = self.session.board.piece_at(change_format_cell(pos_end)-1).symbol()
+        else:
+            cell_occupied = False
+        
+    
         if not self.session.make_move(pos_start+pos_end):
             raise Exception(
                 "Invalid fields pos_start and pos_end. Please try again."
             )
-
+       
+        if cell_occupied: 
+            global pos_board_2 
+            pos_board_2 = pos_board_2+1
+            robot_response = self.robot_conn.send_and_receive( #убираем фигуру на 2 поле
+                robot_request(
+                    1,
+                    change_format_cell(pos_end),
+                    2,
+                    pos_board_2
+                )
+            )
+             
+            if robot_response == 'Done': #ответ робота на перенос фигуры
+                await self.broadcast_success_move()
+            else:
+                raise Exception("The robot failed to make remove figure")
+        
+            color = self.session.players[
+                1 - self.session.current_player
+            ].figures_color.name
+            self.redis_conn.execute( # данные, которые хранятся в БД, что фигура была убрана с доски
+                'RPUSH',
+                self.session.players[
+                    1 - self.session.current_player
+                ].figures_color.name,
+                f"{pos_end}-{change_format_cell_original(pos_board_2)}-{color}-{piece_simbol_end}-1"
+            )
+        
         robot_response = self.robot_conn.send_and_receive(
             robot_request(
                 1,
@@ -74,12 +328,15 @@ class WebSocketServer:
         else:
             raise Exception("The robot failed to make a move")
 
+        color = self.session.players[
+                self.session.current_player
+            ].figures_color.name
         self.redis_conn.execute(
             'RPUSH',
             self.session.players[
                 self.session.current_player
             ].figures_color.name,
-            f"{pos_start}-{pos_end}"
+            f"{pos_start}-{pos_end}-{color}-{piece_simbol_start}-0"
         )
         self.session.current_player = 1 - self.session.current_player
 
@@ -176,3 +433,4 @@ class WebSocketServer:
         async with websockets.serve(self.handle_client, "localhost", 8765):
             print("WebSocket-сервер запущен на порту 8765")
             await asyncio.Future()
+
